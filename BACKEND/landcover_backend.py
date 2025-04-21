f13e74f6fecaa3e2820b639b58433b20b24c4fc7
Hariom Sharma <hariomsharmaji70@gmail.com>
2025-04-21 22:10:00 +0530

    second commit

--- BACKEND/landcover_backend.py
+++ BACKEND/landcover_backend.py
@@ -81,6 +81,12 @@ def classify():
 
     except Exception as e:
         return {"error": str(e)}, 500
+import os
+
+@app.route('/')
+def index():
+    return "Land Cover Classifier Backend is Running!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
